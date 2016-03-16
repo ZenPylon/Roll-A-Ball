d10cfbd0d8948498b947e0fb41f3ed6ca4b0bd6e
Andrew Decker <ajdecker1022@gmail.com>
2016-03-16 18:29:23 -0500

    Add win text and counting text for displaying progress of object counting:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private int count;
 	private Rigidbody rb;
+
 	void Start() {
+		count = 0;
+		SetCountText();
 		rb = GetComponent<Rigidbody>();
 	}
+
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -19,7 +28,18 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Pick Up")) {
 			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
 		}
 	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 10) {
+			winText.text = "You Win!";
+		}
+
+	}
+
 }
 
